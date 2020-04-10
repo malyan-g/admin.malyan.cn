82be008fb0fc63132175b4bccb45e8b1c02e3e83
maliang <maliang@xywy.com>
2020-04-10 18:07:40 +0800

    ml

--- config/web.php
+++ config/web.php
@@ -123,7 +123,7 @@ $config['bootstrap'][] = 'gii';
 $config['modules']['gii'] = [
     'class' => 'yii\gii\Module',
     // uncomment the following to add your IP if you are not connecting from localhost.
-    //'allowedIPs' => ['127.0.0.1', '::1'],
+    'allowedIPs' => ['127.0.0.1', '::1','10.167.216.83'],
 ];
 if (!YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
